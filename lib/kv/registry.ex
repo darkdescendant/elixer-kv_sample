514ed3cf783619b1e43fc7a828addc4436076c07
Joseph Jones <darkdescendant@fastmail.fm>
2016-03-23 12:29:15 -0700

    Initial commit

---
+++ lib/kv/registry.ex
@@ -0,0 +1,40 @@
+defmodule KV.Registry do
+	use GenServer
+
+	@doc"""
+	Start server process
+	"""
+	def start_link () do
+		GenServer.start_link(__MODULE__, :ok, [])
+	end
+
+
+	@doc"""
+	"""
+	def lookup(server, name) do
+		GenServer.call(server, {:lppkup, name})
+	end
+
+	@doc"""
+	"""
+	def create(server, name) do
+		GenServer.cast(server, {:create, name})
+	end
+
+	def init(:ok) do
+		{:ok, %{}}
+	end
+
+	def handle_call({:lookup, name}, _from, names) do
+		{:reply, Map.fetch(names, name), names}
+	end
+
+	def handle_cast({:create, name}, names) do
+		if Map.has_key?(names, name) do
+			{:noreply, names}
+		else
+			{:ok, bucket} = KV.Bucket.start_link()
+			{:noreply, Map.put(names, name, bucket)}
+		end
+	end
+end
